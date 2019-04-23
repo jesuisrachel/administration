90158cec2eb5495e61cdf2c36eb95cd693a62543
hrx <hrx>
2019-04-23 23:13:29 +0800

    globalheader

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
+    <div class="header">
         Header
     </div>
 </template>
+
+<style scoped>
+.header{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
